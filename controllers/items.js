cb2d38298cf90084ce49889d2d9d1196cff9f311
Sami Koskivaara <sami.koskivaara@gmail.com>
2020-06-15 12:05:40 +0300

    Add working button

--- controllers/items.js
+++ controllers/items.js
@@ -25,3 +25,24 @@ exports.getItems = async (req, res, next) => {
     res.status(500).json({ error: "Server error" });
   }
 };
+
+// @desc  Get all items
+// @route GET /api/v1/item
+// @access Public
+exports.getItem = async (req, res, next) => {
+  try {
+    const item = {
+      id: 1,
+      name: "test",
+    };
+
+    return res.status(200).json({
+      success: true,
+      count: 1,
+      data: item,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
+};
--- routes/items.js
+++ routes/items.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { getItems } = require("../controllers/items");
+const { getItems, getItem } = require("../controllers/items");
 
 const router = express.Router();
 
 router.route("/").get(getItems);
+router.route("/1").get(getItem);
 
 module.exports = router;
